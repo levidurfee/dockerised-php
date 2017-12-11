53da64840a6fabe32f3f2feecd4bec3cc8708bea
Levi Durfee <levi.durfee@gmail.com>
2017-12-10 21:39:47 -0500

    setup a swarm

---
+++ php/code/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width" />
+        <title>Levi</title>
+    </head>
+    <body>
+        <h1>
+            <?php
+            echo $_SERVER['HOSTNAME'];
+            ?>
+        </h1>
+        <small>weeee</small>
+    </body>
+</html>
